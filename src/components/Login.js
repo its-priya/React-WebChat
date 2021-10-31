5b84641b51dbd7c8a155d8a9506e0d0ef8e53854
its-priya <priyamissu1999@gmail.com>
2021-10-31 16:16:16 +0530

    Authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
+import "firebase/app";
+import firebase from 'firebase/app';
+import { auth } from './firebase';
 
 const Login = () => {
     return (
         <div id="login-page">
             <div id="login-card">
                 <h1>Welcome to WeChat!</h1>
-                <div className="login-btn google">
+                <div 
+                    className="login-btn google"
+                    onClick= {() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+                >
                     <GoogleOutlined/> Sign in with Google
                 </div>
                 <br/><br/>
-                <div className="login-btn facebook">
+                <div 
+                    className="login-btn facebook"
+                    onClick= {() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
+                >
                     <FacebookOutlined/> Sign in with Facebook
                 </div>
             </div>
